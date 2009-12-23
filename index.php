4fb11763bc5eae62808c46f8a15bbbe294561db4
Jonathan Dalrymple <jonathan@Blackbook.local>
2009-12-23 19:14:09 +0000

    Welcome page explaining the project and locations of files

--- index.php
+++ index.php
@@ -2,3 +2,46 @@
 	require_once('classListing.php');
 	
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<link rel="stylesheet" href="app/css/yui.css" type="text/css" media="screen" title="no title" charset="utf-8">
+		<style type="text/css">
+			body{
+				background: #bbb;
+			}
+			#doc{
+				margin:1em auto;
+			}
+			.panel{
+				background: #eee;
+				padding:1em;
+			}
+			dt{
+				font-weight:bold;
+				font-size: 123.1%;
+			}
+		</style>
+		<title>
+			EWT Level 3 Assignment
+		</title>
+	</head>
+	<body>
+		<div id="doc">
+			<div class="panel">
+				<h1>Jonathan Dalrymple Level 3 EWT Assignment</h1>
+				My Assignment is split into three main parts
+				<dl>
+					<dt>libs</dt>
+					<dd></dd>
+					<dt>app</dt>
+					<dd></dd>
+					<dt>webservice</dt>
+					<dd></dd>
+				</dl>
+				
+			</div>
+		</div>
+	</body>
+</html>
